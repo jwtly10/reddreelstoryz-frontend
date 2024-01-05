40e3f818882f4c149b4ec173ed98753fbe51a0f2
Josh Watley <watleyjosh@gmail.com>
2024-01-05 21:54:17 +0000

    chore: styling updates

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import {AuthProvider} from "./contexts/AuthContext.tsx";
+import { AuthProvider } from './contexts/AuthContext.tsx'
+import { ConfigProvider, theme } from 'antd'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <ConfigProvider
+      theme={{
+        algorithm: theme.darkAlgorithm,
+      }}
+    >
       <AuthProvider>
         <App />
       </AuthProvider>
+    </ConfigProvider>
   </React.StrictMode>,
 )
--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,83 +1,112 @@
-import {useEffect, useState} from "react";
-import * as service from "../BackendService.ts";
-import {Button, Card, Col, Popover, Row} from "antd";
-import video_placeholder from "../assets/video_placeholder.gif"
-import {Container} from "react-bootstrap";
+import { useEffect, useState } from 'react'
+import * as service from '../service/BackendService.ts'
+import { Button, Card, Col, Popover, Row } from 'antd'
+import video_placeholder from '../assets/video_placeholder.gif'
+import { Container } from 'react-bootstrap'
 
 export default function History() {
-    const [videos, setVideos] = useState<VideoData[]>([])
-    const [downloading, setDownloading] = useState(false)
+  const [videos, setVideos] = useState<VideoData[]>([])
+  const [downloading, setDownloading] = useState(false)
 
-    useEffect(() => {
-        service.getHistory()
-            .then((res) => {
-                setVideos(res)
-            })
-            .catch((error) => console.log(error))
-    }, [])
+  useEffect(() => {
+    service
+      .getHistory()
+      .then((res) => {
+        setVideos(res)
+      })
+      .catch((error) => console.log(error))
+  }, [])
 
-    const formatDate = (date: string) => {
-        const dateObj = new Date(date)
-        return dateObj.toLocaleDateString()
-    }
+  const formatDate = (date: string) => {
+    const dateObj = new Date(date)
+    return dateObj.toLocaleDateString()
+  }
 
-    const failedError = (error: string) => {
-        return (
-            <div>
-                <p className="error">{error}</p>
-                <p>If this message isn't useful please get in contact at <br></br>
-                    <a href="contact@ai-content-generator.com">
-                        contact@ai-content-generator.com
-                    </a>
-                </p>
-            </div>
-        )
-    }
-
-    const handleDownloadVideo = async (processId: string) => {
-        setDownloading(true)
-        await service.downloadVideo(processId).then(() => {
-            setDownloading(false)
-        })
-    }
-
+  const failedError = (error: string) => {
     return (
-        <Container>
-            <div className="d-flex mt-5 mb-5 flex-column justify-content-center text-center">
-                <h1>History</h1>
-                <p>Videos will be auto-deleted after 7 days. Download the ones you want to keep!</p>
-            </div>
-        <Row gutter={[16, 16]} justify="center" align="middle">
-            {videos.map((video: VideoData, index) => (
-                <Col xs={24} sm={12} md={8} key={index} >
-                    <Card
-                        hoverable
-                        style={{width: '100%', margin: 'auto'}}
-                        cover={<img alt="example" src={video_placeholder} style={{width: '100%', height: 'auto'}}/>}
+      <div>
+        <p className='error'>{error}</p>
+        <p>
+          If this message isn't useful please get in contact at <br></br>
+          <a href='contact@ai-content-generator.com'>
+            contact@ai-content-generator.com
+          </a>
+        </p>
+      </div>
+    )
+  }
+
+  const handleDownloadVideo = async (processId: string) => {
+    setDownloading(true)
+    await service.downloadVideo(processId).then(() => {
+      setDownloading(false)
+    })
+  }
+
+  return (
+    <Container>
+      <div className='d-flex mt-5 mb-5 flex-column justify-content-center text-center'>
+        <h1>History</h1>
+        <p>
+          Videos will be auto-deleted after 7 days. Download the ones you want
+          to keep!
+        </p>
+      </div>
+      <Row gutter={[16, 16]} justify='center' align='middle'>
+        {videos.map((video: VideoData, index) => (
+          <Col xs={24} sm={12} md={8} key={index}>
+            <Card
+              hoverable
+              style={{ width: '100%', margin: 'auto' }}
+              cover={
+                <img
+                  alt='example'
+                  src={video_placeholder}
+                  style={{ width: '100%', height: 'auto' }}
+                />
+              }
+            >
+              <div style={{ height: 170 }}>
+                {video.state === 'COMPLETED' ? (
+                  <>
+                    <h5>{video.video.fileName}</h5>
+                    <p>{`Length: ${video.video.length} seconds`}</p>
+                    <p>{`Created: ${formatDate(video.video.uploadDate)}`}</p>
+                    <br></br>
+                    <Button
+                      type='primary'
+                      loading={downloading}
+                      onClick={() => handleDownloadVideo(video.video.videoId)}
                     >
-                            <h5>{video.video.fileName}</h5>
-                            <p>{`Length: ${video.video.length} seconds`}</p>
-                            <p>{`Created: ${formatDate(video.video.uploadDate)}`}</p>
-                            <br></br>
-                            {video.state === 'COMPLETED' ? (
-                                <Button type="primary" loading={downloading} onClick={() => handleDownloadVideo(video.video.videoId)}>Download</Button>
-                            ) : video.state === 'FAILED' ? (
-                                <Popover content={failedError(video.error)} title="Video Generation Failed">
-                                    <Button type="primary" danger>
-                                        Failed - Click to see why
-                                    </Button>
-                                </Popover>
-                            ) : (
-                                <Popover content={"This may take a few minutes"} title="Video Generation Processing">
-                                    <Button type="primary" loading={true}>
-                                        {video.state.charAt(0).toUpperCase() + video.state.slice(1).toLowerCase()}
-                                    </Button>
-                                </Popover>
-                            )}
-                    </Card>
-                </Col>
-                ))}
-        </Row>
-        </Container>
-    );
+                      Download
+                    </Button>
+                  </>
+                ) : video.state === 'FAILED' ? (
+                  <Popover
+                    content={failedError(video.error)}
+                    title='Video Generation Failed'
+                  >
+                    <Button type='primary' danger>
+                      Failed - Click to see why
+                    </Button>
+                  </Popover>
+                ) : (
+                  <>
+                    <p>{`Your video is ${video.state.toLowerCase()}.`}</p>
+                    <p>Please come back in a few minutes to see your video.</p>
+                    <br></br>
+
+                    <Button type='primary' loading={true}>
+                      {video.state.charAt(0).toUpperCase() +
+                        video.state.slice(1).toLowerCase()}
+                    </Button>
+                  </>
+                )}
+              </div>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  )
 }
