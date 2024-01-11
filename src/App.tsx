10e3e6af6cd6c0ac407c38e8a8b14840647c63d1
Josh Watley <watleyjosh@gmail.com>
2024-01-11 20:02:14 +0000

    fix: Temp landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useAuth } from "./contexts/AuthContext";
 import { useEffect } from "react";
 import AppRouter from "./router/AppRouter.tsx";
+import LandingPage from "./pages/LandingPage.tsx";
 
 function App() {
   const { login, setAuthToken, setIsAuthenticated, isAuthenticated } =
@@ -15,11 +16,41 @@ function App() {
     }
   }, [login, isAuthenticated]);
 
-  return (
-    <>
-      <AppRouter />
-    </>
-  );
+  // Hide the site and just show the landing page
+  const getSecretAccessKey = () => {
+    const accessKey = localStorage.getItem("accessKey");
+    if (accessKey) {
+      return accessKey;
+    }
+  };
+
+  const KEY = import.meta.env.VITE_ACCESS_KEY as string;
+  const urlParams = new URLSearchParams(window.location.search);
+  const myParam = urlParams.get("accessKey");
+
+  if (getSecretAccessKey() == KEY) {
+    return (
+      <>
+        <AppRouter />
+      </>
+    );
+  } else {
+    if (myParam === KEY) {
+      // Store token in local storage
+      localStorage.setItem("accessKey", myParam);
+      return (
+        <>
+          <AppRouter />
+        </>
+      );
+    } else {
+      return (
+        <div>
+          <LandingPage />
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,7 @@
+export default function LandingPage(){
+    return (
+        <>
+            <h1>COMING SOON</h1>
+        </>
+    )
+}
