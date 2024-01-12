552416e12112dec6d4d5d5bb31703277a1a4f290
Josh Watley <watleyjosh@gmail.com>
2024-01-12 20:13:10 +0000

    fix: Configurable landing page based on env vars

--- src/App.tsx
+++ src/App.tsx
@@ -24,32 +24,43 @@ function App() {
     }
   };
 
-  const KEY = import.meta.env.VITE_ACCESS_KEY as string;
+  const KEY: string | null = import.meta.env.VITE_ACCESS_KEY
+    ? (import.meta.env.VITE_ACCESS_KEY as string)
+    : null;
   const urlParams = new URLSearchParams(window.location.search);
   const myParam = urlParams.get("accessKey");
 
-  if (getSecretAccessKey() == KEY) {
-    return (
-      <>
-        <AppRouter />
-      </>
-    );
-  } else {
-    if (myParam === KEY) {
-      // Store token in local storage
-      localStorage.setItem("accessKey", myParam);
+  // If no KEY then we don't need to check for it
+  if (KEY) {
+    if (getSecretAccessKey() == KEY) {
       return (
         <>
           <AppRouter />
         </>
       );
     } else {
-      return (
-        <div>
-          <LandingPage />
-        </div>
-      );
+      if (myParam === KEY) {
+        // Store token in local storage
+        localStorage.setItem("accessKey", myParam);
+        return (
+          <>
+            <AppRouter />
+          </>
+        );
+      } else {
+        return (
+          <div>
+            <LandingPage />
+          </div>
+        );
+      }
     }
+  } else {
+    return (
+      <>
+        <AppRouter />
+      </>
+    );
   }
 }
 
