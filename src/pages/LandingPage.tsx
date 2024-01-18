343e843088566e4779d627545ae7f8c961e844ef
Josh Watley <watleyjosh@gmail.com>
2024-01-18 21:36:15 +0000

    fix

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,69 +1,69 @@
-import { Container, Navbar, NavItem } from "react-bootstrap";
-import { Button, Card, Form, Input } from "antd";
-import { Footer } from "antd/es/layout/layout";
-import landing_page_video from "../assets/landing_page_video.mp4";
-import logo from "../assets/logo-circle.png";
-import { subscribe } from "../service/BackendService.ts";
-import { useState } from "react";
-import debug from "../utils/debug.ts";
-import ReactPlayer from "react-player";
+import { Container, Navbar, NavItem } from 'react-bootstrap'
+import { Button, Card, Form, Input } from 'antd'
+import { Footer } from 'antd/es/layout/layout'
+import landing_page_video from '../assets/landing_page_video.mp4'
+import logo from '../assets/logo-circle.png'
+import { subscribe } from '../service/BackendService.ts'
+import { useState } from 'react'
+import debug from '../utils/debug.ts'
+import ReactPlayer from 'react-player'
 
 export default function LandingPage() {
-  const [loading, setLoading] = useState(false);
-  const [response, setResponse] = useState("");
-  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false)
+  const [response, setResponse] = useState('')
+  const [error, setError] = useState(false)
   const handleSubmit = (values: any) => {
-    setError(false);
-    setResponse("");
-    debug(values);
-    setLoading(true);
+    setError(false)
+    setResponse('')
+    debug(values)
+    setLoading(true)
     subscribe(values.email).then((res: any) => {
       if (res.response === undefined) {
         if (res.status) {
           if (res.status === 200) {
-            setResponse("Thanks for subscribing!");
-            setLoading(false);
-            return;
+            setResponse('Thanks for subscribing!')
+            setLoading(false)
+            return
           }
         }
       } else {
         if (res.response.status !== 200) {
-          setError(true);
-          setResponse(res.response.data);
-          setLoading(false);
-          return;
+          setError(true)
+          setResponse(res.response.data)
+          setLoading(false)
+          return
         } else {
-          setResponse(res.response.data);
-          setLoading(false);
-          return;
+          setResponse(res.response.data)
+          setLoading(false)
+          return
         }
       }
-    });
-  };
+    })
+  }
 
   return (
     <>
       <div
-        className="d-flex flex-column landing"
-        style={{ minHeight: "100vh" }}
+        className='d-flex flex-column landing'
+        style={{ minHeight: '100vh' }}
       >
         <Navbar
           collapseOnSelect
-          expand="lg"
-          bg="dark"
-          variant="dark"
-          className="bg-body-tertiary mb-3"
+          expand='lg'
+          bg='dark'
+          variant='dark'
+          className='bg-body-tertiary mb-3'
         >
           <Container>
             <Navbar.Brand>
-              <NavItem className="text-decoration-none text-white d-flex">
+              <NavItem className='text-decoration-none text-white d-flex'>
                 <img
                   src={logo}
                   style={{
                     height: 30,
-                    marginRight: "10px",
+                    marginRight: '10px',
                   }}
-                  alt={"Logo img"}
+                  alt={'Logo img'}
                 ></img>
                 ReddReelStoryz
               </NavItem>
@@ -71,8 +71,8 @@ export default function LandingPage() {
           </Container>
         </Navbar>
 
-        <Container className="d-flex align-items-center justify-content-center landing-container m-auto">
-          <Card className="text-center w-75 landing-card border-0 p-0">
+        <Container className='d-flex align-items-center justify-content-center landing-container m-auto'>
+          <Card className='text-center w-75 landing-card border-0 p-0'>
             <h1>Coming Soon!</h1>
             <p>
               🚀 Exciting News! We're cooking up something extraordinary in the
@@ -85,63 +85,64 @@ export default function LandingPage() {
             </p>
             <h3>Stay Tuned for Launch:</h3>
             <p>
-              Be among the first to easily grow your TikTok accounts for{" "}
-              <span className="strong">free</span>!! Sign up now to receive
+              Be among the first to easily grow your TikTok accounts for{' '}
+              <span className='strong'>free</span>!! Sign up now to receive
               exclusive early access and be notified when we release this!
             </p>
             <Form
-              layout="vertical"
-              name="custom-generation-form"
+              layout='vertical'
+              name='custom-generation-form'
               onFinish={handleSubmit}
               onFinishFailed={() => {}}
-              className="col-12 col-sm-12 col-md-12 m-auto"
+              className='col-12 col-sm-12 col-md-12 m-auto'
             >
               <Form.Item
-                name="email"
+                name='email'
                 rules={[
                   { required: true, message: "Don't forget your email! 🙂" },
                 ]}
               >
                 <Input
-                  style={{ textAlign: "center" }}
-                  placeholder="e.g. janedoe@mail.com"
+                  style={{ textAlign: 'center' }}
+                  placeholder='e.g. janedoe@mail.com'
                 />
               </Form.Item>
 
               <Button
-                type="primary"
-                htmlType="submit"
-                className="subscribe-btn mb-1 mt-1"
+                type='primary'
+                htmlType='submit'
+                className='subscribe-btn mb-1 mt-1'
                 loading={loading}
               >
                 Let me know when it releases!
               </Button>
             </Form>
-            <p className={error ? "text-danger" : "text-success"}>{response}</p>
+            <p className={error ? 'text-danger' : 'text-success'}>{response}</p>
             <p>
-              🚀 <strong>ReddReelStoryz - Your gateway to instant reach</strong>{" "}
+              🚀 <strong>ReddReelStoryz - Your gateway to instant reach</strong>{' '}
               🚀
-            </p>{" "}
+            </p>{' '}
             <ReactPlayer
               url={landing_page_video}
-              width="100%"
-              maxheight="250px"
+              width='100%'
+              maxheight='250px'
               controls
             />
           </Card>
         </Container>
-        <div style={{ textAlign: "center", marginTop: "auto" }}>
+        <div style={{ textAlign: 'center', marginTop: 'auto' }}>
           <Footer>
             <a
-              style={{ marginRight: 10, textDecoration: "none" }}
-              target="_blank"
-              href="https://www.tiktok.com/@reddreelstoryz"
+              style={{ marginRight: 10, textDecoration: 'none' }}
+              target='_blank'
+              // href='https://www.tiktok.com/@reddreelstoryz'
+              href=''
             >
-              @ReddReelStoryz
+              ReddReelStoryz
             </a>
           </Footer>
         </div>
       </div>
     </>
-  );
+  )
 }
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,85 +1,85 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import Home from "../pages/Home.tsx";
-import History from "../pages/History.tsx";
-import NavbarComponent from "../components/NavbarComponent.tsx";
-import { useAuth } from "../contexts/AuthContext.tsx";
-import Generate from "../pages/Generate.tsx";
-import { useEffect, useState } from "react";
-import { jwtDecode } from "jwt-decode";
-import SessionExpiredModalComponent from "../components/SessionExpiredModalComponent.tsx";
-import debug from "../utils/debug.ts";
-import LoginModalComponent from "../components/LoginModalComponent.tsx";
-import { Footer } from "antd/es/layout/layout";
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import Home from '../pages/Home.tsx'
+import History from '../pages/History.tsx'
+import NavbarComponent from '../components/NavbarComponent.tsx'
+import { useAuth } from '../contexts/AuthContext.tsx'
+import Generate from '../pages/Generate.tsx'
+import { useEffect, useState } from 'react'
+import { jwtDecode } from 'jwt-decode'
+import SessionExpiredModalComponent from '../components/SessionExpiredModalComponent.tsx'
+import debug from '../utils/debug.ts'
+import LoginModalComponent from '../components/LoginModalComponent.tsx'
+import { Footer } from 'antd/es/layout/layout'
 
 export default function AppRouter() {
-  const { isAuthenticated, logout } = useAuth();
-  const [showPopup, setShowPopup] = useState(false);
-  const [showLoginModal, setShowLoginModal] = useState(false);
+  const { isAuthenticated, logout } = useAuth()
+  const [showPopup, setShowPopup] = useState(false)
+  const [showLoginModal, setShowLoginModal] = useState(false)
 
   useEffect(() => {
     const checkTokenExpiration = () => {
-      debug("Checking session expiration");
-      const token = localStorage.getItem("token");
+      debug('Checking session expiration')
+      const token = localStorage.getItem('token')
       if (token) {
-        const decodedToken = jwtDecode(token);
+        const decodedToken = jwtDecode(token)
         if (decodedToken.exp) {
-          const expirationTimestamp = decodedToken.exp;
-          const expirationDate = new Date(expirationTimestamp * 1000);
-          const expirationDateString = expirationDate.toLocaleString(); // Adjust format as needed
-          debug("Token expiration date:" + expirationDateString);
-          const currentDate = new Date();
-          const currentDateString = currentDate.toLocaleString(); // Adjust format as needed
-          debug("Current date:" + currentDateString);
+          const expirationTimestamp = decodedToken.exp
+          const expirationDate = new Date(expirationTimestamp * 1000)
+          const expirationDateString = expirationDate.toLocaleString() // Adjust format as needed
+          debug('Token expiration date:' + expirationDateString)
+          const currentDate = new Date()
+          const currentDateString = currentDate.toLocaleString() // Adjust format as needed
+          debug('Current date:' + currentDateString)
 
           if (decodedToken.exp * 1000 < Date.now()) {
-            logout();
-            setShowPopup(true);
+            logout()
+            setShowPopup(true)
           }
         }
       } else {
         // We have no auth token. just ensure they are logged out
-        logout();
+        logout()
       }
 
       // If no token. Assume user is not logged in
-    };
+    }
 
-    const secondsToCheck = 10;
+    const secondsToCheck = 10
     const expirationCheckInterval = setInterval(
       checkTokenExpiration,
       secondsToCheck * 1000,
-    );
+    )
 
     return () => {
-      clearInterval(expirationCheckInterval);
-    };
-  }, [logout]);
+      clearInterval(expirationCheckInterval)
+    }
+  }, [logout])
 
   const handleClose = () => {
-    setShowLoginModal(false);
-  };
+    setShowLoginModal(false)
+  }
 
   return (
     <div
       style={{
-        display: "flex",
-        flexDirection: "column",
-        minHeight: "100vh",
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
       }}
     >
       <BrowserRouter>
         <NavbarComponent />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path='/' element={<Home />} />
           {isAuthenticated ? (
             <>
-              <Route path="/generate" element={<Generate />} />
-              <Route path="/history" element={<History />} />
+              <Route path='/generate' element={<Generate />} />
+              <Route path='/history' element={<History />} />
             </>
           ) : (
             <>
-              <Route path="/generate" element={<Navigate to="/" />} />
-              <Route path="/history" element={<Navigate to="/" />} />
+              <Route path='/generate' element={<Navigate to='/' />} />
+              <Route path='/history' element={<Navigate to='/' />} />
             </>
           )}
         </Routes>
@@ -87,25 +87,26 @@ export default function AppRouter() {
         <SessionExpiredModalComponent
           showModal={showPopup}
           handleOk={() => {
-            setShowPopup(false);
-            logout();
-            setShowLoginModal(true);
+            setShowPopup(false)
+            logout()
+            setShowLoginModal(true)
           }}
         />
         <LoginModalComponent handleClose={handleClose} open={showLoginModal} />
-        <div style={{ textAlign: "center", marginTop: "auto" }}>
-          <Footer style={{ marginTop: "5rem" }}>
+        <div style={{ textAlign: 'center', marginTop: 'auto' }}>
+          <Footer style={{ marginTop: '5rem' }}>
             <a
-              style={{ marginRight: 10, textDecoration: "none" }}
-              target="_blank"
-              href="https://www.tiktok.com/@reddreelstoryz"
+              style={{ marginRight: 10, textDecoration: 'none' }}
+              target='_blank'
+              // href="https://www.tiktok.com/@reddreelstoryz"
+              href=''
             >
-              @ReddReelStoryz
+              ReddReelStoryz
             </a>
-            ©{new Date().getFullYear()}{" "}
+            ©{new Date().getFullYear()}{' '}
           </Footer>
         </div>
       </BrowserRouter>
     </div>
-  );
+  )
 }
